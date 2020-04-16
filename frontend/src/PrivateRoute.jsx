1679d81be5be1a61dc2f0538e15235dca53dc417
brutzki <emiln17@ru.is>
2020-04-16 19:53:12 +0000

    Changed the PrivateRoutes component to be more readable

--- frontend/src/PrivateRoute.jsx
+++ frontend/src/PrivateRoute.jsx
@@ -1,21 +1,37 @@
 // Dependencies
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 // Source
 import { useAuth } from './context/auth';
 import { views } from './config';
 
-const PrivateRoute = ({ component: Component, ...rest }) => {
+const PrivateRoute = (props) => {
+  const { component: Component, func, path } = props;
   const { authTokens } = useAuth();
+
+  const handlePageChange = (parent) => {
+    if (authTokens) {
+      if (func(views[path])) {
+        return <Component parent={parent} />;
+      }
+    }
+    return <Redirect to="/" />;
+  };
+
   return (
     <Route
-      {...rest}
-      render={(props) => (authTokens ? (
-        rest.func(views[rest.path]) ? <Component {...props} /> : <Redirect to="/" />
-      ) : <Redirect to="/" />)}
+      func={func}
+      path={path}
+      render={(parent) => handlePageChange(parent)}
     />
   );
 };
 
+PrivateRoute.propTypes = {
+  component: PropTypes.func.isRequired,
+  func: PropTypes.func.isRequired,
+  path: PropTypes.string.isRequired,
+};
 export default PrivateRoute;
