5c14588bfcb4cdc49872379db6f38d915374c366
emilnewel <emiln17@ru.is>
2020-04-24 15:03:35 +0000

    test

--- frontend/src/components/Modals/UserDetailsModal/index.jsx
+++ frontend/src/components/Modals/UserDetailsModal/index.jsx
@@ -1,19 +1,13 @@
 import React from 'react';
 import './UserDetailsModal.css';
+import { Button } from '@material-ui/core';
 import UserDetailsForm from '../../Forms/UserDetailsForm';
 
 const UserDetailsModal = (props) => (
-  <div className="userDetailsModal">
-    <div className="content-left">
-      <h1> User Details Modal </h1>
-      <UserDetailsForm />
-    </div>
-    <div className="buttons-right">
-      <button type="button" className="save-changes-btn"> Loka/Opna Aðgang </button>
-      <button type="button" className="save-changes-btn"> Change password </button>
-    </div>
+  <>
+    <UserDetailsForm />
 
-  </div>
+  </>
 );
 
 export default UserDetailsModal;
