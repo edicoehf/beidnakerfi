85b1d7aca9b0525707093cd965482ad765e0f8b3
brutzki <emiln17@ru.is>
2020-04-28 15:48:08 +0000

    Okay site is up

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+// import { CssBaseline } from '@material-ui/core';
 import App from './App/App';
 
+
+const theme = createMuiTheme({
+  palette: {
+    primary: { 500: '#990000' },
+    secondary: { A400: '#6D6D6D' },
+  },
+});
+
 ReactDOM.render(
-  <App />,
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>,
   document.getElementById('root'),
 );
