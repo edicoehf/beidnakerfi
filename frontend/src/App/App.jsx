84a92b6fb3ca5f70ab394745d353a288dc3d889d
brutzki <emiln17@ru.is>
2020-04-28 14:52:03 +0000

    Moved ThemeProvider and CSSBaseline

--- frontend/src/App/App.jsx
+++ frontend/src/App/App.jsx
@@ -1,26 +1,13 @@
 // Dependencies
 import React from 'react';
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 
 // Source from us
-import { CssBaseline } from '@material-ui/core';
 import Routes from '../Routes';
 
 // Service
 function App() {
-  const theme = createMuiTheme({
-    palette: {
-      primary: { 500: '#990000' },
-      secondary: { A400: '#6D6D6D' },
-    },
-  });
-
-  console.log('in App');
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Routes />
-    </ThemeProvider>
+    <Routes />
   );
 }
 
