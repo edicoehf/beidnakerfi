c22197cf5291d0591967700d7b3bbb6044d5b19b
brutzki <emiln17@ru.is>
2020-05-06 15:39:46 +0000

    Changed tables to now use material-ui and removed viewCustomers since it was cancelled

--- frontend/src/components/Lists/ChequeList/index.jsx
+++ frontend/src/components/Lists/ChequeList/index.jsx
@@ -1,41 +1,80 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import '../Lists.css';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
 import { statusCodes } from '../../../config';
 import { sortBy } from '../../../services';
 import { getCheque } from '../../../services/apiGateway';
+
+
+const useStyles = makeStyles((themes) => ({
+  container: {
+    marginTop: themes.spacing(10),
+    width: '90%',
+  },
+  tableHead: {
+    backgroundColor: themes.palette.primary.main,
+  },
+  tableHeadCell: {
+    color: 'white',
+    fontSize: '14px',
+    padding: themes.spacing(1),
+  },
+  tableCell: {
+    fontSize: '14px',
+    padding: themes.spacing(1),
+  },
+  tableRow: {
+
+    '&:hover': {
+      cursor: 'pointer',
+    },
+  },
+}));
+
 
 const ChequeList = (props) => {
   const {
     setDrawerOpen, setCheque, chequeList, setChequeList,
   } = props;
+  const classes = useStyles();
   const [desc, setDesc] = useState(true);
 
   const handleClick = async (cheque) => {
-    const detaildCheque  = await getCheque(cheque.code);
+    const detaildCheque = await getCheque(cheque.code);
     await setCheque(detaildCheque.data);
     setDrawerOpen(true);
-
   };
+
   const sort = async (item, subItem) => {
     const sorted = await sortBy(chequeList, item, subItem, setDesc, desc);
     setChequeList(sorted);
   };
-  return (
 
-    <table className="table">
-      <thead>
-        <tr>
-          <th onClick={() => sort('code')}>Beiðnanúmer</th>
-          <th onClick={() => sort('seller', 'name')}>Söluaðili</th>
-          <th onClick={() => sort('department', 'name')}>Kostnaðarstaður</th>
-          <th onClick={() => sort('user', 'username')}>Úttektaraðili</th>
-          <th onClick={() => sort('created')}>Dagsetning</th>
-          <th onClick={() => sort('status')}>Staða</th>
-        </tr>
-      </thead>
-      <tbody>
-        {
+  return (
+    <TableContainer className={classes.container} component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow className={classes.tableHead}>
+            <TableCell className={classes.tableHeadCell} onClick={() => sort('code')}>
+              Beiðnanúmer
+            </TableCell>
+            <TableCell className={classes.tableHeadCell} align="center" onClick={() => sort('seller', 'name')}>Söluaðili</TableCell>
+            <TableCell className={classes.tableHeadCell} align="center" onClick={() => sort('department', 'name')}>Kostnaðarstaður</TableCell>
+            <TableCell className={classes.tableHeadCell} align="center" onClick={() => sort('user', 'username')}>Úttektaraðili</TableCell>
+            <TableCell className={classes.tableHeadCell} align="center" onClick={() => sort('created')}>Dagsetning</TableCell>
+            <TableCell className={classes.tableHeadCell} align="center" onClick={() => sort('status')}>Staða</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {
           chequeList.filter((cheque) => cheque.code.includes(props.query)).map((cheque) => {
             const {
               code, status, created,
@@ -47,19 +86,22 @@ const ChequeList = (props) => {
             const readableDate = new Date(created).toLocaleString('en-GB');
 
             return (
-              <tr onClick={() => handleClick(cheque)} className="row" key={code}>
-                <td>{ code }</td>
-                <td>{ sellerName }</td>
-                <td>{ deptName }</td>
-                <td>{ username }</td>
-                <td>{ readableDate }</td>
-                <td>{ statusCodes[status] }</td>
-              </tr>
+              <TableRow className={classes.tableRow} onClick={() => handleClick(cheque)} key={code}>
+                <TableCell className={classes.tableCell}>
+                  { code }
+                </TableCell>
+                <TableCell className={classes.tableCell} align="center">{ sellerName }</TableCell>
+                <TableCell className={classes.tableCell} align="center">{ deptName }</TableCell>
+                <TableCell className={classes.tableCell} align="center">{ username }</TableCell>
+                <TableCell className={classes.tableCell} align="center">{ readableDate }</TableCell>
+                <TableCell className={classes.tableCell} align="center">{ statusCodes[status] }</TableCell>
+              </TableRow>
             );
           })
         }
-      </tbody>
-    </table>
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 };
 
